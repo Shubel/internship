3f48cb75a57ad6737b8a473ffabfdbd93ab77474
Sunny <shubelislam.diu@gmail.com>
2021-09-22 15:37:07 +0600

    checkbox checked

---
+++ text.js
@@ -0,0 +1,27 @@
+function myFunction1() {
+    var checkBox = document.getElementById("myCheck1");
+    var text = document.getElementById("text1");
+    if (checkBox.checked == true){
+      text.style.display = "block";
+    } else {
+       text.style.display = "none";
+    }
+  }
+function myFunction2() {
+    var checkBox = document.getElementById("myCheck2");
+    var text = document.getElementById("text2");
+    if (checkBox.checked == true){
+      text.style.display = "block";
+    } else {
+       text.style.display = "none";
+    }
+  }
+function myFunction3() {
+    var checkBox = document.getElementById("myCheck3"); 
+    var text = document.getElementById("text3");
+    if (checkBox.checked == true){
+      text.style.display = "block";
+    } else {
+       text.style.display = "none";
+    }
+  }
